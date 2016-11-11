381ab1c832ec43bccf88cdaceefb8d2219f25bd8
Takeshi Koizumi <takeshi_koizumi@kronos-jp.net>
2016-11-11 20:49:44 +0900

    ログインビュー UI調整

--- src/app/Main.js
+++ src/app/Main.js
@@ -32,12 +32,6 @@ class Main extends Component {
     };
   }
 
-  handleRequestClose = () => {
-    this.setState({
-      open: false,
-    });
-  }
-
   handleTouchTap = () => {
     this.setState({
       open: true,
@@ -56,18 +50,9 @@ class Main extends Component {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div style={styles.container}>
-          <Dialog
-            open={this.state.open}
-            title="Super Secret Password"
-            actions={standardActions}
-            onRequestClose={this.handleRequestClose}
-          >
-            1-2-3-4-5
-          </Dialog>
-          <h1>Material-UI</h1>
-          <h2>example project</h2>
+          <h1>Firebase TODO</h1>
           <RaisedButton
-            label="Super Secret Password"
+            label="Googleアカウントでログイン"
             secondary={true}
             onTouchTap={this.handleTouchTap}
           />
